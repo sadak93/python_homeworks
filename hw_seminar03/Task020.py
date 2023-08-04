7d022eaefcef1444b812dbe268bee522977dfca3
Sadak <s.sadak02@gmail.com>
2023-08-04 16:51:31 +0600

    дополнения

--- hw_seminar03/Task020.py
+++ hw_seminar03/Task020.py
@@ -55,3 +55,27 @@ for i in word:
             if i in points_ru[j]:
                 count = count + j
 print(count)
+
+alpha = {'AEIOULNSTRАВЕИНОРСТ': 1,
+         'DGДКЛМПУ': 2,
+         'BCMPБГЁЬЯ': 3,
+         'FHVWYЙЫ': 4,
+         'KЖЗХЦЧ': 5,
+         'JXШЭЮ': 8,
+         'QZФЩЪ': 10}
+
+word = input('Введите слово: ')
+total = 0
+new_alpha = {}
+for letters, score in alpha.items():
+    new_alpha.update(dict.fromkeys(letters, score))
+
+for char in word.upper():
+    total += new_alpha.get(char)
+
+# for char in word.upper():
+#     for letters, score in alpha.items():
+#         if char in letters:
+#             total += score
+
+print(f'Слово {word} весит {total} очков')
